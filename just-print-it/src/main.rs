65cdfc5d47def5395da1ede27fffd14e5d0eee37
Eduardo Braun <eduardotbraun@gmail.com>
2021-08-29 12:19:41 -0300

    Add functions

--- just-print-it/src/main.rs
+++ just-print-it/src/main.rs
@@ -1,7 +1,27 @@
-use tracing::info;
+use tracing::{error, info};
 use tracing_subscriber;
+
+fn fn_a () {
+    info!("hello from fn_a");
+    for i in (0..3).rev() {
+        let res = sub_one(i);
+        info!(num=i, res, "{}-1={}", i, res);
+    }
+}
+
+fn sub_one (value: u8) -> u8 {
+    info!("hello from sub_one");
+    match value.checked_sub(1) {
+        Some(i) => i,
+        None => {
+            error!("subtraction failed!");
+            0
+        },
+    }
+}
 
 fn main() {
     tracing_subscriber::fmt::init();
-    info!("hello world");
+    info!("hello from main");
+    fn_a();
 }
